e2bcfe2ab768e02522748822d2d43b4e4ced8acf
Islam Ashyrmuradov <i.ashyrmuradov@lotta.local>
2024-07-05 17:29:07 +0500

    10

---
+++ public/assets/js/app.js
@@ -0,0 +1,25 @@
+
+$(".slick-carousel").slick({
+    dots:false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows:  $(".sales-arrow"),
+    prevArrow:'<div class="prev-arrow"><img src="/assets/images/Arrow_right.svg" alt=""></div>',
+    nextArrow:'<div class="next-arrow"><img src="/assets/images/Arrow_left.svg" alt=""></div>',
+    
+    
+
+  });
+
+
+  // $(".slick-prev").slick-prev({
+  //   type:boolean,
+  //   default:false, 
+  // })
+  
+
+
+
+  
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,85 +8,318 @@
     <title>Flacon</title>
     <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/slick-1.8.1/slick/slick.css') }}">
+
 
 </head>
 
 <body>
-    <div class="discount">
+    <div class="discount " style="  background-color: #FF0080; padding:15px 0px 10px 0px;">
         <div class="container d-flex justify-content-between align-items-center">
-            <p style="color: white; margin-bottom: 0;">Только до конца месяца <span style="">скидка 20%</span> на
-                все наливные духи</p>
-            <button class="btn p-0" style="background: none; border: none;">
-                <img src="{{ asset('/assets/images/cross.png') }}" alt="Close" style="width: 24px; height: 24px;">
+            <p>Только до конца месяца <span style="font-family:suisseintl-bold">скидка 20%</span> на все
+                наливные духи</p>
+            <button class="btn p-0" style="background: none; border: none;"> <img
+                    src="{{ asset('/assets/images/cross.png') }}" alt="Close" style="width: 24px; height: 24px;">
             </button>
         </div>
     </div>
-    <div class="container">
-        <div class="row row-cols-2 row-cols-lg-5 g-3 me-auto ">
-            <div class=" p-3 dropdown">
-                <li class="services"><a href="/">Условия работы <img class="dropdown-icon"
-                            src="{{ asset('/assets/images/arrowdown.png') }}" alt="">
-                    </a>
-                    <div class="dropdown-content">
-                        <a href="/">Как сделать заказ</a>
-                        <a href="/">Доставка/оплата</a>
-                        <a href="/">Система скидок</a>
-                        <a href="/">Рекдамные материалы</a>
-                        <a href="/">Сертификаты</a>
-                        <a href="/">Отзывы</a>
-                        <a href="/">Политика конфиденциальности</a>
-                    </div>
-                </li>
-            </div>
-            <div class="col">
-                <div class="p-3 "><a href="">Франшиза</a></div>
-            </div>
-            <div class="col">
-                <div class="p-3"><a href="">Блог</a></div>
-            </div>
-            <div class="col">
-                <div class="p-3"><a href="">О нас</a></div>
-            </div>
-            <div class="col">
-                <div class="p-3"><a href=""> Контакты</a></div>
+    <div class="content-width" style="background-color: #F1F4F9">
+        <div class="container justify-content-between align-items-center ">
+            <div class="row justify-content-between " style="padding: 13px 0">
+                <div class=" col-2  dropdown" style="width: fit-content">
+                    <li class="services"><a href="/" class="about">Условия работы <img class="dropdown-icon"
+                                src="{{ asset('/assets/images/arrowdown.png') }}" alt="">
+                        </a>
+                        <div class="dropdown-content ">
+                            <a href="/">Как сделать заказ</a>
+                            <a href="/">Доставка/оплата</a>
+                            <a href="/">Система скидок</a>
+                            <a href="/">Рекдамные материалы</a>
+                            <a href="/">Сертификаты</a>
+                            <a href="/">Отзывы</a>
+                            <a href="/">Политика конфиденциальности</a>
+                        </div>
+                    </li>
+                </div>
+                <div class="col-2">
+                    <div class=" "><a href="" class="about">Франшиза</a></div>
+                </div>
+                <div class="col-2">
+                    <div class=""><a href="" class="about">Блог</a></div>
+                </div>
+                <div class="col-2">
+                    <div class=""><a href="" class="about">О нас</a></div>
+                </div>
+                <div class="col-2 text-end">
+                    <div class=""><a href="" class="about"> Контакты</a></div>
+                </div>
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <img src="{{ asset('/assets/images/logo.png') }} ">
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="services"><a href="/">8(800)-100-60-41 <img class="dropdown-icon" src="{{ asset('/assets/images/arrowdown.png') }}" alt=""></a>
+
+    <div class="container" style="padding: 20px 0px">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-3" style="padding-left:20px">
+                <a href=""><img src="{{ asset('/assets/images/logo.png') }} "></a>
+            </div>
+
+            <div class="col-4 ">
+                <div class="searchdiv">
+                    <input class="form" type="search" placeholder="Поиск по товарам" aria-label="Search1">
+                    <div class="searchbtn">
+                    </div>
+                </div>
+            </div>
+            <div class="col-2 dropdown">
+                <ul class="nav">
+                    <li class="services"><a href="/" style="font-family:SuisseIntl-Medium" class="about">8
+                            (800)-100-60-41 <img class="dropdown-icon" src="{{ asset('/assets/images/arrowdown.png') }}"
+                                alt=""></a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li class="num"><a class="dropdown-item" style="padding-top: 20px"
-                                    href="#">8(800)-100-60-41</a></li>
-                            <li style="padding: 0px 0px 15px 13px !important; font-size:small">Звонок бесплатный</li>
-                            <li class="num"><a class="dropdown-item" href="#">+7 (903) 845 78 02 <img
-                                        class="phoneicon" src="{{ asset('/assets/images/whatsapp2.png') }}"> <img
-                                        src="{{ asset('/assets/images/viber.png') }}"></a></li>
-                            <li class="num"><a class="dropdown-item" href="#">+7 (909) 260 97 97 <img
-                                        class="phoneicon" src="{{ asset('/assets/images/whatsapp2.png') }}"> <img
-                                        src="{{ asset('/assets/images/viber.png') }}"></a></li>
-                            <li class="num"><a class="dropdown-item" href="#">+7 (903) 841 66 77 <img
-                                        class="phoneicon" src="{{ asset('/assets/images/whatsapp2.png') }}"> <img
-                                        src="{{ asset('/assets/images/viber.png') }}"></a></li>
-                            <li style="padding: 15px 0px 0px 17px ; font-size:small" >Время работы:</li>
-                            <li style="padding-left: 17px">ПН-ПТ 9:30-18:00</li>
+                            <li class="num"><a class="dropdown-item"href="#" style="font-family:SuisseIntl-Medium"> 8
+                                    (800)-100-60-41</a></li>
+                            <li style="font-size:small">Звонок бесплатный </li>
+                            <div class="d-flex align-items-center" style="padding-top: 15px">
+                                <li class="num" style=""><a class="dropdown-item" href="#"
+                                        style="font-family:SuisseIntl-Medium">+7 (903) 845
+                                        78 02</a></li>
+                                <div class="whatsapp-icon" style="margin-left: 5px">
+                                </div>
+                                <div class="viber-icon" style="margin-left: 5px">
+                                </div>
+                            </div>
+
+                            <div class="d-flex align-items-center" style="padding-top: 15px">
+                                <li class="num" style=""><a class="dropdown-item" href="#"
+                                        style="font-family:SuisseIntl-Medium">+7 (903) 845
+                                        78 02</a></li>
+                                <div class="whatsapp-icon" style="margin-left: 5px">
+                                </div>
+                                <div class="viber-icon" style="margin-left: 5px">
+                                </div>
+                            </div>
+
+                            <div class="d-flex align-items-center" style="padding-top: 15px">
+                                <li class="num" style=""><a class="dropdown-item" href="#"
+                                        style="font-family:SuisseIntl-Medium">+7 (903)
+                                        845 78 02</a></li>
+                                <div class="whatsapp-icon" style="margin-left: 5px">
+                                </div>
+                                <div class="viber-icon" style="margin-left: 5px">
+                                </div>
+                            </div>
+
+
+                            <li style="font-size:small; padding-top:15px">Время работы:</li>
+                            <li style="padding-bottom:14px" style="font-family:SuisseIntl-Medium">ПН-ПТ 9:30-18:00
+                            </li>
                         </ul>
                     </li>
                 </ul>
+            </div>
 
+            <div class="col-3 text-end">
+                <div class="d-flex justify-content-end">
+                    <a href="">
+                        <div class="user-icon" style="margin-right: 20px">
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="favorite-icon" style="margin-right: 20px">
+                        </div>
+                    </a>
+                    <div class="bag-prc d-flex align-items-center">
+                        <a href="">
+                            <div class="cart-with-badge">
+                                <div class="bag-icon" style="margin-right: 5px">
+                                </div>
+                                <div class="badge">2</div>
+                            </div>
+                        </a>
+                        <p style="margin: 0">1350₽</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+    </div>
+
+    <div class=" container-fluid" style="border-top: 2px solid #F1F4F9;border-bottom: 2px solid #F1F4F9">
+        <div class="container justify-content-between align-items-center" style="padding: 19px 0;">
+            <div class="row justify-content-between">
+                <div class=" col-2 dropdown" style="padding-left: 18px">
+                    <div class=""><a href="" class="about-parfum">Наливная информация </a></div>
+                </div>
+                <div class="col-2" style="padding-left:15px">
+                    <div class=" "><a href="" class="about-parfum">Флаконы для духов </a></div>
+                </div>
+                <div class="col-2">
+                    <div class=""><a href="" class="about-parfum">SPA-косметика</a></div>
+                </div>
+                <div class="col-2">
+                    <div class=""><a href="" class="about-parfum">Ароматы для дома</a></div>
+                </div>
+                <div class="col-2 ">
+                    <div class=""><a href="" class="about-parfum"> Аксессуары/Упаковка</a></div>
+                </div>
+                <div class="col-2 text-end">
+                    <div class=""><a href="" class="about-parfum"> Готовые решения</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- BODY --}}
+    {{-- start --}}
+
+    <div class="hot-offers">
+        <div class="offers-title d-flex">
+            <img src="{{ asset('/assets/images/hot.svg') }}" alt="">
+            <a href="">
+                <p>Текущие акции</p>
+            </a>
+        </div>
+    </div>
+    <div class="content-banner">
+        <div class="container">
+            <div class="banner">
+                <p>Производство и продажа парфюмерии, аромадиффузоров и флаконов</p>
+                <div class="wholesale">ОПТОМ от 5000 руб.</div>
+                <button>В Каталог</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="margin-top:104px">
+        <div class="sales-header">
+            <div class="row justify-content-between">
+                <div class=" col-9 d-flex">
+                    <p>Лидеры продаж</p>
+                </div>
+                <div class=" col-3 sales-arrow  text-end d-flex">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" style="margin-top:35px">
+        <div class="slick-carousel">
+            <div class=" flex-column custom-slide" >
+                <img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt="">
+                <p style="font-size: 15px; ">Shermadini House(LAB parfum) №159 (100мл)</p>
+                <div>
+                    <p style="font-size:12px ">Ночной/Апельсин/Зима/Осень</p>
+                    <button>Узнать цену</button>
+                </div>
+            </div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 2.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 3.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 4.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt=""></div>
+            <div class="custom-slide"><img src="{{ asset('/assets/images/Rectangle 1.svg') }}" alt=""></div>
+        </div>
+    </div>
+
+    <div class="btn-center">
+        <button>В каталог</button>
+    </div>
+
+    <div class="container">
+        <div class="banner">
+            <p>Получите подарок</p>
+            <div class="wholesale">ОПТОМ от 5000 руб.</div>
+            <button style="width: 370px; align-items-center ">Получить</button>
+        </div>
+    </div>
+
+    {{-- end --}}
+
+    <footer>
+        <div class="container justify-content-between ">
+            <div class="row justify-content-between align-items-start footer-catalog ">
+
+                <div class="col-3 footer-content d-flex flex-column">
+                    <p class="footer-title">Каталог</p>
+                    <a href="">Флаконы для духов</a>
+                    <a href="">Наливная парфюмерия</a>
+                    <a href="">СПА косметика</a>
+                    <a href=""> Ароматы для дома</a>
+                    <a href="">Аксессуары/Упаковка</a>
+                </div>
+                <div class="col-3 footer-content d-flex flex-column">
+                    <p class="footer-title">Условия работы</p>
+                    <a href="">Как сделать заказ</a>
+                    <a href="">Доставка/оплата</a>
+                    <a href="">Скидочная система</a>
+                    <a href="">Диллерство</a>
+                    <a href="">Рекламные материалы</a>
+                    <a href="" style="margin-bottom: 30px">Сертификаты</a>
+                </div>
+                <div class="col-3 footer-content d-flex flex-column">
+                    <p class="footer-title">Контакты</p>
+                    <p style="opacity: 0.75"> Время работы: с 09:30 до 18:00, ПН-ПТ</p>
+                    <a href="">8 (800) 100 60 41 - звонок по РФ бесплатно </a>
+                    <a href="">+7 (903) 845 78 02 - Whatsapp/Viber</a>
+                    <a href="">+7 (909) 260 97 97 - Whatsapp/Viber</a>
+                    <a href="">+7 (903) 841 66 77 - Whatsapp/Viber</a>
+                </div>
+                <div class="col-3 footer-content d-flex flex-column">
+                    <p class="footer-title">О нас</p>
+                    <p style="opacity: 0.75">Производсто и Оптовая продажа Парфюма и Флаконов по России и СНГ </p>
+                    <p style="opacity: 0.75">Достовляем закакзы в любой город России, Беларуси,Казахстана и Армении</p>
+                    <div class="footer-contact-ic d-flex justify-content-between" style="margin-top: 56px">
+                        <a href="" class="footer-ic">
+                            <div class="vk"></div>
+                        </a>
+                        <a href="" class="footer-ic">
+                            <div class="youtube"></div>
+                        </a>
+                        <a href="" class="footer-ic">
+                            <div class="telegram"></div>
+                        </a>
+                        <a href="" class="footer-ic">
+                            <div class="whatsapp"></div>
+                        </a>
+                        <a href="" class="footer-ic">
+                            <div class="viber"></div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="container logos d-flex" style=" padding:30px 0">
+            <div class="col-4" style="padding-top: ">
+                <img src="{{ asset('/assets/images/logo.svg') }} ">
+            </div>
+            <div class="col-4">
+                <img src="{{ asset('/assets/images/Frame 3181.svg') }} ">
+            </div>
+            <div class="col-4">
+                <img src="{{ asset('/assets/images/Frame 3184.svg') }} ">
+            </div>
+        </div>
+        <div class="container company-since d-flex justify-content-between" style="padding-top: 15px">
+            <div class="col4">
+                <p>Работаем с 2016 года.</p>
+            </div>
+            <div class="col4">
+                <p>Работаем с 2016 года.</p>
+            </div>
+            <div class="col4">
+                <p>Работаем с 2016 года.</p>
+            </div>
+        </div>
+    </footer>
+
     <script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('/assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('/assets/slick-1.8.1/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/app.js') }}"></script>
+
 
 </body>
 
